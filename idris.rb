882020f216b5df46482ce266b4a0c5ebf718dfc9
Jared Roesch <roeschinc@gmail.com>
2013-12-13 22:42:04 -0800

    update to working version

--- idris.rb
+++ idris.rb
@@ -5,7 +5,7 @@ class Idris < Formula
   url 'https://github.com/jroesch/Idris-dev/archive/0.9.9.3.tar.gz'
   sha1 '19f5ff7d1e78da6cd722f1c883a4cd807d2db8b5'
 
-  depends_on 'llvm' => if_gcc ? ["-use-gcc"] : []
+  depends_on 'llvm'
   depends_on 'boehmgc'
   depends_on 'libffi'
   depends_on 'gmp'
@@ -14,8 +14,8 @@ class Idris < Formula
   depends_on 'cabal-install'
 
   def install
-    system 'cabal install alex' if `which alex` =~ /alex/
     system 'cabal update'
+    system 'cabal install alex' if `which alex` =~ /alex/
     system 'make'
   end
 
