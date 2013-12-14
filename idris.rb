7b243361fbc64837670e8948922227366503f65e
Jared Roesch <roeschinc@gmail.com>
2013-12-14 01:13:31 -0800

    fix cc option on llvm

--- idris.rb
+++ idris.rb
@@ -4,22 +4,24 @@ class Idris < Formula
   homepage 'www.idris-lang.org'
   url 'https://github.com/jroesch/Idris-dev/archive/v0.9.10.1.tar.gz'
   sha1 '2d426d081d37292c6432bd6c68041b2ad1963b68'
-
-  ghc_info = eval(`ghc --info`.gsub("(", "[").gsub(")", "]").gsub("\n", " "))
-  cc = if ghc_info[2][1] =~ /gcc/
-    "--cc=gcc-4.2"
-  else
-    "--cc=clang"
-  end
+ 
 
   depends_on 'apple-gcc42'
-  depends_on 'llvm' => cc
-  depends_on 'boehmgc'
-  depends_on 'libffi'
   depends_on 'gmp'
-  depends_on 'pkg-config'
+  depends_on 'libffi'
   depends_on 'ghc'
   depends_on 'cabal-install'
+
+  # ghc_info = eval(`ghc --info`.gsub("(", "[").gsub(")", "]").gsub("\n", " "))
+  # cc = if ghc_info[2][1] =~ /gcc/
+  #   "--cc=gcc-4.2"
+  # else
+  #   "--cc=clang"
+  # end
+
+  depends_on 'llvm' => "--cc=gcc-4.2"
+  depends_on 'boehmgc'
+  depends_on 'pkg-config'
 
   def install
     # Compute number of cores
