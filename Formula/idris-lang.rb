2e9ec818e64d6ffd918779ee9b13e50a32488561
Jared Roesch <roeschinc@gmail.com>
2013-12-13 21:37:47 -0800

    commit dirty file

--- Formula/idris-lang.rb
+++ Formula/idris-lang.rb
@@ -5,7 +5,7 @@ class IdrisLang < Formula
   url 'https://github.com/jroesch/Idris-dev/archive/0.9.9.3.tar.gz'
   sha1 '19f5ff7d1e78da6cd722f1c883a4cd807d2db8b5'
 
-  depends_on 'llvm' #=> if_gcc ? "-use-gcc" : ""
+  depends_on 'llvm' => if_gcc ? ["-use-gcc"] : []
   depends_on 'boehmgc'
   depends_on 'libffi'
   depends_on 'gmp'
@@ -24,14 +24,6 @@ class IdrisLang < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test Idris-dev`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "--version"`.
-    system "false"
+    install
   end
 end
